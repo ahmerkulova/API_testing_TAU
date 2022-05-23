47c40216c44576fbc251c061ce8b87761258459c
Arina <ahmerkulova@gmail.com>
2022-05-23 12:26:49 +0300

    add test to assert POST method and module for functions

---
+++ main/people_functions.py
@@ -0,0 +1,30 @@
+import requests
+from assertpy.assertpy import assert_that
+from config import BASE_URI
+
+from json import dumps
+from uuid import uuid4
+
+
+def create_new_person():
+    """ Function to create new user. json.dumps() is used to convert python dict to json string
+     uuid4.uuid4 is used to get a unique last name to ensure we don’t have conflicting data """
+    print('str(uuid4())', str(uuid4()))
+    unique_last_name = f'User {str(uuid4())}'
+    payload = dumps({
+        'fname': 'New',
+        'lname': unique_last_name
+    })
+    print('payload', payload)
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    response = requests.post(url=BASE_URI, data=payload, headers=headers)
+    assert_that(response.status_code, description='Person not created').is_equal_to(requests.codes.no_content)
+    return unique_last_name
+
+
+def search_created_user_in(peoples, last_name):
+    """ Function to return bool of there is new user in the list of users """
+    return [person for person in peoples if person['lname'] == last_name]
--- tests/people_test.py
+++ tests/people_test.py
@@ -1,12 +1,22 @@
-import requests
-from assertpy.assertpy import assert_that
-from config import BASE_URI
+from main.people_functions import *
 
 
 def test_read_all_has_kent():
-    """ Function to assert that in response of GET request one of first names contains 'Kent' """
+    """ Test to assert that one of first names in response of GET request contains 'Kent' """
     response = requests.get(BASE_URI)
     assert_that(response.status_code).is_equal_to(requests.codes.ok)
     response_text = response.json()
     first_names = [people['fname'] for people in response_text]
     assert_that(first_names).contains('Kent')
+
+
+def test_new_person_can_be_added():
+    """ Test to assert that new added user can be found in the list of users """
+    unique_last_name = create_new_person()
+    peoples = requests.get(BASE_URI).json()
+    is_new_user_created = search_created_user_in(peoples, unique_last_name)
+    assert_that(is_new_user_created).is_not_empty()
+
+
+
+
