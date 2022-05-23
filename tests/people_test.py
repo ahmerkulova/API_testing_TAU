b0b4b22708a373f1bc5c43225efb0cd7a1614ad5
Arina <ahmerkulova@gmail.com>
2022-05-23 13:27:04 +0300

    add test to assert DELETE method

--- tests/people_test.py
+++ tests/people_test.py
@@ -11,12 +11,18 @@ def test_read_all_has_kent():
 
 
 def test_new_person_can_be_added():
-    """ Test to assert that new added user can be found in the list of users """
+    """ Test to assert that new added user (POST method) can be found in the list of users """
     unique_last_name = create_new_person()
     peoples = requests.get(BASE_URI).json()
     is_new_user_created = search_created_user_in(peoples, unique_last_name)
     assert_that(is_new_user_created).is_not_empty()
 
 
-
-
+def test_created_person_can_be_deleted():
+    """ Test to assert that new added can be deleted (DELETE method) """
+    persons_last_name = create_new_person()
+    peoples = requests.get(BASE_URI).json()
+    newly_created_user = search_created_user_in(peoples, persons_last_name)[0]
+    delete_url = f'{BASE_URI}/{newly_created_user["person_id"]}'
+    response = requests.delete(delete_url)
+    assert_that(response.status_code).is_equal_to(requests.codes.ok)
