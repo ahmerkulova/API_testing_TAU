03835233414730b84bd05c2271266706f732b88b
Arina <ahmerkulova@gmail.com>
2022-05-25 11:07:50 +0300

    refactor covid client tests

--- test/covid_test.py
+++ test/covid_test.py
@@ -1,6 +1,7 @@
 import requests
 from assertpy import assert_that
 from lxml import etree
+
 from config import COVID_TRACKER_HOST
 
 
@@ -17,7 +18,7 @@ def test_covid_cases_have_crossed_a_million():
     assert_that(int(total_cases)).is_greater_than(1000000)
 
 
-def test_overall_covid_cases_match_sum_of_total_cases_by_country():
+def test_covid_cases_sum_by_country_matches_total():
     """ Test to assert sum of country cases (found via XPath in a loop) matches overall sum """
     response = requests.get(f'{COVID_TRACKER_HOST}/api/v1/summary/latest')
     response_xml = response.text
@@ -30,4 +31,4 @@ def test_overall_covid_cases_match_sum_of_total_cases_by_country():
     for region in search_for(xml_tree):
         cases_by_country += int(region.text)
 
-    assert_that(overall_cases).is_greater_than(cases_by_country)
+    assert_that(overall_cases).is_equal_to(cases_by_country)
